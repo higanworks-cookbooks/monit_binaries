d48f2f31d7338fd8dae1f9223da0cf3974776f72
sawanoboly <sawanoboriyu@higanworks.com>
2012-09-25 16:32:48 +0900

    auto start enable

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "sawanoboriyu@higanworks.com"
 license          "MIT"
 description      "Installs/Configures monit_binaries"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
