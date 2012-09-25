a2b25c843ad117989ab6b1d29e7403ce7801b6e4
sawanoboly <sawanoboriyu@higanworks.com>
2012-09-25 16:20:46 +0900

    action :nothing

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,8 +13,9 @@ end
 service "monit" do
   provider Chef::Provider::Service::Upstart
   supports :restart => true, :reload => true
-  action [:enable, :start ]
-  only_if File.exists?("/usr/sbin/monit")
+#  action [:enable, :start ]
+  action :nothing
+#  only_if File.exists?("/usr/sbin/monit")
 end
 
 %w{/etc/monit /etc/monit/conf.enable /etc/monit/conf.avail /usr/local/src/monit}.each do |w|
