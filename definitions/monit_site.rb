bbb503a4e7c87e717d1fec284e1554ca23966f1d
sawanoboly <sawanoboriyu@higanworks.com>
2012-10-01 23:35:04 +0900

    add def monit_site

---
+++ definitions/monit_site.rb
@@ -0,0 +1,24 @@
+# cookbook Name:: monit_binaries
+# Definition:: monit_site
+# Author:: Yukihiko Sawanobori <sawanoboriyu@higanorks.com>
+#
+# Refernce: Opscode-cookbook::nginx
+# Copyright 2011-2012, HiganWorks LLC
+#
+
+define :monit_site, :enable => true do
+  if params[:enable]
+    execute "monitensite #{params[:name]}" do
+      command "ln -s /etc/monit/conf.avail/#{params[:name]}.conf /etc/monit/conf.enable/#{params[:name]}.conf"
+      notifies :reload, resources(:service => "monit")
+      not_if do ::File.symlink?("/etc/monit/conf.enabled/#{params[:name]}.conf") end
+    end
+  else
+    execute "nxdissite #{params[:name]}" do
+      command "rm /etc/monit/conf.enable/#{params[:name]}.conf"
+      notifies :reload, resources(:service => "monit")
+      only_if do ::File.symlink?("/etc/monit/conf.enabled/#{params[:name]}.conf") end
+    end
+  end
+end
+
