5746c6d0058307dfffeba60e4a8f7c5a91c53d21
sawanoboly <sawanoboriyu@higanworks.com>
2012-10-16 12:08:59 +0900

    Workaround older chef: FC017: LWRP does not notify when updated

--- providers/default.rb
+++ providers/default.rb
@@ -3,6 +3,7 @@ action :enable do
     command "/usr/local/sbin/monitensite #{new_resource.name}"
     notifies :reload, resources(:service => "monit")
     not_if do ::File.symlink?("#{node['monit']['dir']}/conf.enable/#{new_resource.name}") end
+    new_resource.updated_by_last_action(true)
   end
 end
 
@@ -11,6 +12,7 @@ action :disable do
     command "/usr/local/sbin/monitdisite #{new_resource.name}"
     notifies :reload, resources(:service => "monit")
     only_if do ::File.symlink?("#{node['monit']['dir']}/conf.enable/#{new_resource.name}") end
+    new_resource.updated_by_last_action(true)
   end
 end
 
